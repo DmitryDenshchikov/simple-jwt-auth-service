ba2433638b574afe2db95a566acb36e49749d44b
Dmitry Denshchikov <mr.dd33@yandex.ru>
2023-12-20 09:49:43 +0200

    Initial commit

---
+++ src/main/java/denshchikov/dmitry/simpleeventmanagementservice/SimpleEventManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package denshchikov.dmitry.simpleeventmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEventManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEventManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/denshchikov/dmitry/simpleeventmanagementservice/SimpleEventManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package denshchikov.dmitry.simpleeventmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEventManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
